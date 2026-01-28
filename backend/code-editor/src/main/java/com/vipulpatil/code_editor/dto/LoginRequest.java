84ab20939d6638f5d55754ee5dbbe7f0fa6fbbdd
Vipul Patil <vipulpatil6342@gmail.com>
2026-01-28 18:59:37 +0530

    Modify validation for password

--- backend/code-editor/src/main/java/com/vipulpatil/code_editor/dto/LoginRequest.java
+++ backend/code-editor/src/main/java/com/vipulpatil/code_editor/dto/LoginRequest.java
@@ -13,6 +13,5 @@ public class LoginRequest {
     private String identity;
 
     @NotBlank(message = "Password is required")
-    @Size(min = 8, max = 64, message = "Password must be between 8 and 64 characters")
     private String password;
 }
